125e2c97809aea75a7655858e5ad977f1329f87b
Oyale Peter <oyalepeter@gmail.com>
2020-06-25 09:53:32 +0100

    Changes Made

--- lib/screens/client/chatpage.dart
+++ lib/screens/client/chatpage.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
 
 class ChatPage extends StatefulWidget {
   @override
@@ -66,50 +68,89 @@ class _ChatPageState extends State<ChatPage> {
           child: Stack(
             children: <Widget>[
               Container(
-                width: width,
-                height: height,
-                child: user == null? Container(): StreamBuilder(
-                  stream: Firestore.instance.collection("chats").document(user.uid).collection('userChats').orderBy('timeStamp').snapshots(),
-                  builder: (context,snapshot){
-                    if(snapshot.hasData){
-                      if(snapshot.data.documents.length < 1){
-                        return Center(child: Text("No Chats Yet"),);
-                      }
-                      else{
-                        return ListView.builder(
-                            itemCount: snapshot.data.documents.length,
-                            itemBuilder: (context,ind){
-                              return ListTile(
-                                leading: snapshot.data.documents[ind]["userId"] != user.uid?CircleAvatar(
-                                  child: Text("A"),
-                                ): null,
-                                trailing: snapshot.data.documents[ind]["userId"] == user.uid?CircleAvatar(
-                                  child: Text("U"),
-                                ): null,
-                                title: Container(
-                                  width: width * 0.3,
-                                  padding: EdgeInsets.all(8),
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey.withOpacity(0.8),
-                                      borderRadius: BorderRadius.circular(15)
-                                  ),
-                                  child: Text("${snapshot.data.documents[ind]['message']}"),
-                                ),
+                  width: width,
+                  height: height,
+                  padding: EdgeInsets.only(left: 8,right: 8),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          width: width,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              SizedBox(width: 50,),
+                              Text("${DateTime.now().hour} : ${DateTime.now().minute} ${DateTime.now().hour>12? "PM": "AM"}"),
+                              IconButton(
+                                onPressed: ()=> Navigator.of(context).pop(),
+                                icon: Icon(Icons.cancel,color: Colors.black,),
+                              )
+                            ],
+                          ),
+                        ),
+                        user == null? Center(child: CircularProgressIndicator(),): StreamBuilder(
+                          stream: Firestore.instance.collection("chats").document(user.uid).collection('userChats').orderBy('timeStamp').snapshots(),
+                          builder: (context,snapshot){
+                            if(snapshot.hasData){
+                              if(snapshot.data.documents.length < 1){
+                                return Center(child: Text("No Chats Yet"),);
+                              }
+                              else{
+                                return ListView.separated(
+                                    physics: NeverScrollableScrollPhysics(),
+                                    shrinkWrap: true,
+                                    separatorBuilder: (context,ind)=> SizedBox(height: 5,),
+                                    itemCount: snapshot.data.documents.length,
+                                    itemBuilder: (context,ind){
+                                      return Row(
+                                        mainAxisAlignment: snapshot.data.documents[ind]["userId"] == user.uid? MainAxisAlignment.end: MainAxisAlignment.start,
+                                        children: <Widget>[
+                                          snapshot.data.documents[ind]["userId"] != user.uid?CircleAvatar(
+                                            child: Text("A"),
+                                            backgroundColor: Colors.blueAccent,
+                                          ): Container(),
+                                          SizedBox(width: 10,),
+                                          Column(
+                                            crossAxisAlignment: snapshot.data.documents[ind]["userId"] == user.uid? CrossAxisAlignment.end: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Container(
+                                                padding: EdgeInsets.all(15),
+                                                decoration: BoxDecoration(
+                                                    gradient: snapshot.data.documents[ind]["userId"] != user.uid? null: LinearGradient(
+                                                        begin: Alignment.topCenter,
+                                                        end: Alignment.bottomCenter,
+                                                        colors: [Colors.blue,Colors.blue.withOpacity(0.9), Colors.greenAccent]),
+                                                    color: snapshot.data.documents[ind]["userId"] == user.uid? null: Colors.grey.withOpacity(0.4),
+                                                    borderRadius: snapshot.data.documents[ind]["userId"] == user.uid?BorderRadius.only(bottomRight: Radius.circular(15),bottomLeft: Radius.circular(15),topLeft: Radius.circular(15)): BorderRadius.only(topRight: Radius.circular(15),bottomRight: Radius.circular(15),bottomLeft: Radius.circular(15))
+                                                ),
+                                                child: Text("${snapshot.data.documents[ind]['message']}",style: TextStyle(color: snapshot.data.documents[ind]["userId"] == user.uid?Colors.white: Colors.black),),
+                                              ),
+                                              Text("${timeago.format(DateTime.fromMillisecondsSinceEpoch(snapshot.data.documents[ind]['timeStamp']), locale: 'en_short')}")
+                                            ],
+                                          ),
+                                          SizedBox(width: 10,),
+                                          snapshot.data.documents[ind]["userId"] == user.uid?CircleAvatar(
+                                            child: Text("U"),
+                                          ): Container(),
+                                        ],
+                                      );
+                                    });
+                              }
+                            }
+                            else{
+                              return Center(child: CircularProgressIndicator());
+                            }
 
-                              );
-                            });
-                      }
-                    }
-                    else{
-                      return Center(child: CircularProgressIndicator());
-                    }
-
-                  },
-                )
+                          },
+                        ),
+                      ],
+                    ),
+                  )
               ),
               Positioned(
                 bottom: 0,
                 child: Container(
+                  color: Colors.white,
                   width: width,
                   padding: EdgeInsets.all(10),
                   child: Row(
@@ -119,7 +160,7 @@ class _ChatPageState extends State<ChatPage> {
                         child: TextField(
                           controller: _newMessageController,
                           decoration: InputDecoration(
-                            hintText: "Your message"
+                              hintText: "Your message"
                           ),
                         ),
                       ),
