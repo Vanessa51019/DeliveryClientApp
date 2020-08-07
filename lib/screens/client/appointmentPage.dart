a0dfd75b663b7ca5dec950c41b38a6cf8c5de890
Oyale Peter <oyalepeter@gmail.com>
2020-08-07 22:35:27 +0100

    appointment system edited

--- lib/screens/client/appointmentPage.dart
+++ lib/screens/client/appointmentPage.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:delivery_app/screens/client/doctorDetails.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 
 class AppointmentPage extends StatefulWidget {
   @override
@@ -6,6 +10,27 @@ class AppointmentPage extends StatefulWidget {
 }
 
 class _AppointmentPageState extends State<AppointmentPage> {
+
+
+
+  int activeSpeciality = 1;
+  List doctors = [];
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getAllDoctorsData();
+  }
+  
+  _getAllDoctorsData() async {
+    doctors = (await Firestore.instance.collection("doctors").where("speciality",isEqualTo: "S"+ activeSpeciality.toString()).getDocuments()).documents;
+    setState(() {
+
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,60 +63,79 @@ class _AppointmentPageState extends State<AppointmentPage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            height: MediaQuery.of(context).size.width*0.25,
-                            width: MediaQuery.of(context).size.width*0.25,
-                            decoration: BoxDecoration(
-                              color: Color(0xffb1e2e2),
-                              borderRadius: BorderRadius.circular(10)
+                      InkWell(
+                        onTap: (){
+                            activeSpeciality = 1;
+                            _getAllDoctorsData();
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              height: MediaQuery.of(context).size.width*0.25,
+                              width: MediaQuery.of(context).size.width*0.25,
+                              decoration: BoxDecoration(
+                                color: Color(0xffb1e2e2),
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              child: Icon(Icons.favorite),
                             ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 10),
-                            child: Text("Lorem ipsum", style: TextStyle(fontSize: 13, color: Color(0xffb1e2e2)),)
-                          )
-                        ],
+                            Padding(
+                              padding: EdgeInsets.only(top: 10),
+                              child: Text("Lorem ipsum", style: TextStyle(fontSize: 13, color: Color(0xffb1e2e2)),)
+                            )
+                          ],
+                        ),
                       ),
 
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            height: MediaQuery.of(context).size.width*0.25,
-                            width: MediaQuery.of(context).size.width*0.25,
-                            decoration: BoxDecoration(
-                              color: Color(0xff00b4b9),
-                              borderRadius: BorderRadius.circular(10)
+                      InkWell(
+                        onTap: (){
+                            activeSpeciality = 2;
+                            _getAllDoctorsData();
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              height: MediaQuery.of(context).size.width*0.25,
+                              width: MediaQuery.of(context).size.width*0.25,
+                              decoration: BoxDecoration(
+                                color: Color(0xff00b4b9),
+                                borderRadius: BorderRadius.circular(10)
+                              ),
                             ),
-                          ),
 
-                          Padding(
-                            padding: EdgeInsets.only(top: 10),
-                            child: Text("Lorem ipsum", style: TextStyle(fontSize: 13, color: Color(0xff00b4b9)),)
-                          ),
-                        ],
+                            Padding(
+                              padding: EdgeInsets.only(top: 10),
+                              child: Text("Lorem ipsum", style: TextStyle(fontSize: 13, color: Color(0xff00b4b9)),)
+                            ),
+                          ],
+                        ),
                       ),
 
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            height: MediaQuery.of(context).size.width*0.25,
-                            width: MediaQuery.of(context).size.width*0.25,
-                            decoration: BoxDecoration(
-                              color: Color(0xffb1e2e2),
-                              borderRadius: BorderRadius.circular(10)
+                      InkWell(
+                        onTap: (){
+                            activeSpeciality = 3;
+                            _getAllDoctorsData();
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              height: MediaQuery.of(context).size.width*0.25,
+                              width: MediaQuery.of(context).size.width*0.25,
+                              decoration: BoxDecoration(
+                                color: Color(0xffb1e2e2),
+                                borderRadius: BorderRadius.circular(10)
+                              ),
                             ),
-                          ),
 
-                          Padding(
-                            padding: EdgeInsets.only(top: 10),
-                            child: Text("Lorem ipsum", style: TextStyle(fontSize: 13, color: Color(0xffb1e2e2)),)
-                          ),
-                        ],
+                            Padding(
+                              padding: EdgeInsets.only(top: 10),
+                              child: Text("Lorem ipsum", style: TextStyle(fontSize: 13, color: Color(0xffb1e2e2)),)
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
@@ -118,7 +162,7 @@ class _AppointmentPageState extends State<AppointmentPage> {
                         removeTop: true,
                         context: context,
                         child: ListView.builder(
-                          itemCount: 10,
+                          itemCount: doctors.length,
                           itemBuilder: availability,
                         ),
                       ),
@@ -135,27 +179,30 @@ class _AppointmentPageState extends State<AppointmentPage> {
   }
 
   Widget availability(BuildContext context, int index){
-    return Container(
-      height: 50,
-      margin: EdgeInsets.only(bottom: 8),
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 15),
-            child: ClipRRect(
-              child: Image.asset("assets/images/profile.jpg", width: 50, height: 45, fit: BoxFit.cover),
-              borderRadius: BorderRadius.circular(15),
+    return InkWell(
+      onTap: ()=> Navigator.of(context).push(CupertinoPageRoute(builder: (context)=> DoctorDetails(doctors[index]))),
+      child: Container(
+        height: 50,
+        margin: EdgeInsets.only(bottom: 8),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 15),
+              child: ClipRRect(
+                child: Image.asset("assets/images/profile.jpg", width: 50, height: 45, fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(15),
+              ),
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Dr.Lorem Ipsum", style: TextStyle(color: Colors.white),),
-              Text("Lorem Ipsum - Hospital", style: TextStyle(fontSize: 13, color: Colors.white),),
-            ],
-          ),
-        ],
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Dr.${doctors[index]["name"]}", style: TextStyle(color: Colors.white),),
+                Text("${doctors[index]["speciality"]}", style: TextStyle(fontSize: 13, color: Colors.white),),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
